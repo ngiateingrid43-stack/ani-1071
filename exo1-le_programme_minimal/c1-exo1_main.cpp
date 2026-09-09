f5c03982d5dfcf9c1b825e85cc5e573abf17105d
NGIATE KAMNANG INGRID <ngiateingrid43@gmail.com>
2026-09-09 02:35:42 +0100

    Deuxieme commit : saisie du code source du fichier c1-exo1_main.cpp et compilation

---
+++ exo1-le_programme_minimal/c1-exo1_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    
+    // Affichage de mon nom sur une ligne
+    std::cout << "NGIATE KAMNANG INGRID" << std::endl;
+    // Affichage de ma ville sur une ligne
+    std::cout << "YAOUNDE" << std::endl;
+
+    return 0;
+}
