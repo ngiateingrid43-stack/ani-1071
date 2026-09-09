97eb6cc8c9fae6dd34794fe365f46a7cc7f2b2ea
NGIATE KAMNANG INGRID <ngiateingrid43@gmail.com>
2026-09-09 04:31:27 +0100

    ajout de exo4

--- chapitre-01/exo3-lire_trois_erreurs/c1-exo3_main.cpp
+++ chapitre-01/exo3-lire_trois_erreurs/c1-exo3_main.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 int main(int argc, char* argv[]) {
     // Affichage de mon nom sur une ligne
-    std::cout << "NGIATE KAMNANG INGRID";
+    std::cout << "NGIATE KAMNANG INGRID\n";
     // Affichage de ma ville sur une ligne
     std::cout << "YAOUNDE";
     return 0;
---
+++ chapitre-01/exo4-le_code_de_sortie/c1-exo4_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(int argc, char* argv[]) {
+    // Affichage de mon nom sur une ligne
+    std::cout << "NGIATE KAMNANG INGRID\n";
+    // Affichage de ma ville sur une ligne
+    std::cout << "YAOUNDE";
+    return 3;
+}
+/*Le code de sortie est la valeur numérique qu'un programme renvoie au système à sa terminaison, 
+transmise indépendamment de tout texte affiché à l'écran. Par convention, 0 signale un succès et toute autre valeur signale un échec 
+dans notre cas c'est 3 et ca peut signifier une erreur */
