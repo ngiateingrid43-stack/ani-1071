1292403e9c1adcbfc0f22f9893d7f3275be9760b
NGIATE KAMNANG INGRID <ngiateingrid43@gmail.com>
2026-09-09 03:11:46 +0100

    modifications du style

--- chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
+++ chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
@@ -5,7 +5,6 @@ int main(int argc, char* argv[]) {
     // Affichage de mon nom sur une ligne
     std::cout << "NGIATE KAMNANG INGRID" << std::endl;
     // Affichage de ma ville sur une ligne
-    std::cout << "YAOUNDE" << std::endl;
-
+    std::cout << "YAOUNDE";
     return 0;
 }
